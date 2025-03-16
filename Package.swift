a69c9a15c91a38dc083045552d08741376849835
Joshua Peek <josh@users.noreply.github.com>
2025-03-16 13:11:17 -0700

    Back to swift 5.8

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.8
 
 import PackageDescription
 
 let package = Package(
   name: "tccpolicy",
   platforms: [
-    .macOS(.v14)
+    .macOS(.v13)
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0")
