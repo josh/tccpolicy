d2a8b75f6c8e6be6f97fdf576764474e5dfa0b9c
Joshua Peek <josh@users.noreply.github.com>
2025-03-15 18:45:02 -0700

    Get working on Swift 5.8

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.8
 
 import PackageDescription
 
 let package = Package(
   name: "tccpolicy",
   platforms: [
-    .macOS(.v15)
+    .macOS(.v13)
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0")
---
+++ Sources/tccpolicy/StandardErrorStream.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct StandardErrorStream: TextOutputStream {
+  func write(_ string: String) {
+    if let data = string.data(using: .utf8) {
+      FileHandle.standardError.write(data)
+    }
+  }
+}
+
+var stderr = StandardErrorStream()
