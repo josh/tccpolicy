19d8d694280dbd6bb0d67c09553d7b425a63c434
Joshua Peek <josh@users.noreply.github.com>
2025-03-15 19:38:59 -0700

    Back to Swift 5.9 at least

--- Sources/tccpolicy/Policy+Request.swift
+++ Sources/tccpolicy/Policy+Request.swift
@@ -6,28 +6,14 @@ extension Policy {
     let eventStore = EKEventStore()
 
     if self.calendar == true {
-      if try await eventStore.requestAccess(to: .event) == false {
+      if try await eventStore.requestFullAccessToEvents() == false {
         print("warn: Calendar access denied", to: &stderr)
       }
     }
 
     if self.reminders == true {
-      if try await eventStore.requestAccess(to: .reminder) == false {
+      if try await eventStore.requestFullAccessToReminders() == false {
         print("warn: Reminders access denied", to: &stderr)
-      }
-    }
-  }
-}
-
-extension EKEventStore {
-  func requestAccess(to entityType: EKEntityType) async throws -> Bool {
-    return try await withCheckedThrowingContinuation { continuation in
-      self.requestAccess(to: entityType) { granted, error in
-        if let error = error {
-          continuation.resume(throwing: error)
-        } else {
-          continuation.resume(returning: granted)
-        }
       }
     }
   }
