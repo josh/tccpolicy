2b7b49b7e5245d65063ef5dcd7f054695c33a31c
Joshua Peek <josh@users.noreply.github.com>
2025-03-17 21:50:41 -0700

    Add initial test

---
+++ Tests/test.bash
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# client="/bin/date"
+# TODO: Add stub program to use as test client
+client="com.mitchellh.ghostty"
+policy_file="$(mktemp)"
+echo '{"Reminders": true}' >"$policy_file"
+
+echo "1..5"
+
+if swift build >/dev/null 1>&2; then
+  echo "ok 1 - swift build: pass"
+else
+  echo "not ok 1 - swift build: fail"
+  exit 1
+fi
+
+tccpolicy=".build/arm64-apple-macosx/debug/tccpolicy"
+
+if $tccpolicy dump --client "$client" | jq --exit-status 'has("Reminders") == false' 1>/dev/null; then
+  echo "ok 2 - tccpolicy dump: clean"
+else
+  echo "not ok 2 - tccpolicy dump: dirty"
+fi
+
+$tccpolicy request --client "$client" --policy "$policy_file"
+
+if $tccpolicy check --client "$client" --policy "$policy_file"; then
+  echo "ok 3 - tccpolicy check: pass"
+else
+  echo "not ok 3 - tccpolicy check: fail"
+fi
+
+if $tccpolicy dump --client "$client" | jq --exit-status '.Reminders == true' 1>/dev/null; then
+  echo "ok 4 - tccpolicy dump: pass"
+else
+  echo "not ok 4 - tccpolicy dump: fail"
+fi
+
+# FIXME: This should accept "Reminders" not "kTCCServiceReminders"
+if $tccpolicy reset --client "$client" --service "kTCCServiceReminders" 1>&2; then
+  echo "ok 5 - tccpolicy reset: pass"
+else
+  echo "not ok 5 - tccpolicy reset: fail"
+fi
