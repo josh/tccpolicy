e93ac0f020775885de2f0fb3a09ff313e6f99407
Joshua Peek <josh@users.noreply.github.com>
2025-03-17 23:04:51 -0700

    fmt

--- Tests/test.bash
+++ Tests/test.bash
@@ -13,41 +13,41 @@ echo '{"Reminders": true}' >"$policy_file"
 echo "1..6"
 
 if swift build >/dev/null 1>&2; then
-  echo "ok 1 - swift build: pass"
+	echo "ok 1 - swift build: pass"
 else
-  echo "not ok 1 - swift build: fail"
-  exit 1
+	echo "not ok 1 - swift build: fail"
+	exit 1
 fi
 
 tccpolicy=".build/arm64-apple-macosx/debug/tccpolicy"
 
 if $tccpolicy dump --client "$client" | jq --exit-status 'has("Reminders") == false' 1>/dev/null; then
-  echo "ok 2 - tccpolicy dump: clean"
+	echo "ok 2 - tccpolicy dump: clean"
 else
-  echo "not ok 2 - tccpolicy dump: dirty"
+	echo "not ok 2 - tccpolicy dump: dirty"
 fi
 
 if $tccpolicy request --client "$client" --policy "$policy_file" 1>&2; then
-  echo "ok 3 - tccpolicy request: pass"
+	echo "ok 3 - tccpolicy request: pass"
 else
-  echo "not ok 3 - tccpolicy request: fail"
-  exit 1
+	echo "not ok 3 - tccpolicy request: fail"
+	exit 1
 fi
 
 if $tccpolicy check --client "$client" --policy "$policy_file"; then
-  echo "ok 4 - tccpolicy check: pass"
+	echo "ok 4 - tccpolicy check: pass"
 else
-  echo "not ok 4 - tccpolicy check: fail"
+	echo "not ok 4 - tccpolicy check: fail"
 fi
 
 if $tccpolicy dump --client "$client" | jq --exit-status '.Reminders == true' 1>/dev/null; then
-  echo "ok 5 - tccpolicy dump: pass"
+	echo "ok 5 - tccpolicy dump: pass"
 else
-  echo "not ok 5 - tccpolicy dump: fail"
+	echo "not ok 5 - tccpolicy dump: fail"
 fi
 
 if $tccpolicy reset --client "$client" --service "Reminders" 1>&2; then
-  echo "ok 6 - tccpolicy reset: pass"
+	echo "ok 6 - tccpolicy reset: pass"
 else
-  echo "not ok 6 - tccpolicy reset: fail"
+	echo "not ok 6 - tccpolicy reset: fail"
 fi
